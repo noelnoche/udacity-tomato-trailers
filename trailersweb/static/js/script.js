fedbcd25e1c2ad99b6466cb12673f2b0bc8fb301
Noel Noche <noche@Noels-MacBook-Pro.local>
2020-10-27 20:00:49 -0700

    Update script.js
    Format code

--- trailersweb/static/js/script.js
+++ trailersweb/static/js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   function removeVideoContent() {
-    // Remove the src so the player itself gets removed, a$movieTileis the 
+    // Remove the src so the player itself gets removed, as this is the 
     // only reliable way to ensure the video stops playing in IE
     $('#trailer-video').attr('src', '');
     $('#trailer-video-container').empty();
@@ -19,7 +19,7 @@ $(document).ready(function() {
     var storyline = curMovieTile.attr('data-storyline')
     var link = curMovieTile.attr('data-link')
     var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-      
+
     $('#trailer-video-container').empty().append($('<iframe></iframe>', {
       'id': 'trailer-video',
       'type': 'text-html',
@@ -27,18 +27,18 @@ $(document).ready(function() {
       'frameborder': 0
     }));
 
-    var element = '<div class="movie-info"><p>STORY: ' + storyline + 
-                  '</p><hr><a href="' + link + '">IMDb Link: ' + link + '</a></div>';
-    $('.modal-content').append(element)   
+    var element = '<div class="movie-info"><p>STORY: ' + storyline +
+      '</p><hr><a href="' + link + '">IMDb Link: ' + link + '</a></div>';
+    $('.modal-content').append(element)
   }
 
   var $movieTile = $('.movie-tile');
 
-  $movieTile.on('click', function() {
+  $movieTile.on('click', function () {
     addVideoContent($(this));
   });
-  
-  
+
+
   // Animate in the movies when the page loads
   $(document).ready(function () {
     $movieTile.hide().first().show('fast', function showNext() {
